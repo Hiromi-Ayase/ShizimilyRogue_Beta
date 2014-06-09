54a8d46b3af3f5bd714740c65dca9fbead7e66d3
Hiromi Ayase <hiromi.ayase@gmail.com>
2014-06-10 02:02:09 +0900

    移植した

---
+++ ShizimilyRogue/scripts/controller.ts
@@ -0,0 +1,46 @@
+﻿module ShizimilyRogue.Controller {
+    // ダンジョンの論理サイズ
+    var WIDTH = 25;
+    var HEIGHT = 25;
+
+    export class Game {
+        private sceneManager: View.SceneManager;
+        private map: Dungeon.Map;
+
+        constructor() {
+        }
+
+        // フレームごとに呼ばれる
+        public updateFrame(): void {
+        }
+
+        // 入力ごとに呼ばれる
+        public inputHandler(code:Dungeon.Input): void {
+        }
+
+        // ゲームの開始
+        public start(): void {
+            // Dungeon(Model)とSceneManager(View)の作成
+            this.sceneManager = new View.SceneManager();
+            var t = this;
+
+            // Initialize view
+            View.Scene.init(function () {
+                t.newGame();
+            });
+        }
+
+        private newGame(): void {
+            // Map生成
+            this.map = new Dungeon.Map(WIDTH, HEIGHT);
+            // SceneをGameSceneに
+            var floorTable = this.map.getTable(Common.Layer.Floor);
+            var groundTable = this.map.getTable(Common.Layer.Ground);
+            this.sceneManager.scene = new View.GameScene(floorTable, groundTable);
+        }
+
+        // ゲームの終了判定
+        private isEnd(): void {
+        }
+    }
+} 
---
+++ ShizimilyRogue/scripts/scene.ts
@@ -0,0 +1,41 @@
+﻿module ShizimilyRogue.View {
+    // ゲーム画面のサイズ
+    var VIEW_WIDTH = 640;
+    var VIEW_HEIGHT = 480;
+    var FPS = 30;
+
+    // シーン
+    export class Scene extends enchant.Scene {
+        static IMAGES: string[] = ["../images/wall_01.png", "../images/floor_01.png"];
+        static IMAGE_WALL: enchant.Surface;
+        static IMAGE_FLOOR: enchant.Surface;
+
+        static game: enchant.Core;
+
+        static init(onloadHandler:Function) {
+            enchant();
+            Scene.game = new enchant.Core(VIEW_WIDTH, VIEW_HEIGHT);
+            Scene.game.fps = FPS;
+            Scene.game.preload(Scene.IMAGES);
+            Scene.game.onload = () => {
+                Scene.IMAGE_WALL = Scene.game.assets[Scene.IMAGES[0]];
+                Scene.IMAGE_FLOOR = Scene.game.assets[Scene.IMAGES[1]];
+                onloadHandler();
+            };
+            Scene.game.start();
+        }
+    }
+
+    export class SceneManager {
+        set scene(scene: Scene) {
+            Scene.game.replaceScene(scene);
+        }
+    }
+
+    class OpeningScene extends Scene {
+    }
+
+    class EndingScene extends Scene {
+    }
+
+}
